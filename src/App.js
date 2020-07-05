1c0ce04ba2292ec706ceab41710b16d827ceb797
Norberto Mantohac <nmantohac82098@gmail.com>
2020-07-04 20:35:16 -0700

    created new React app and modified App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    monsters: []
+  }
+
+  async componentDidMount() {
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(users => this.setState({ monsters: users }));
+
+    try {
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+      this.setState({ monsters: data });
+    } catch (e) {
+      if (e) throw e;
+    }
+  }
+
+  render() {
+    const { monsters } = this.state;
+    return (
+      <div className="App">
+        { monsters.map(monster => {
+          return <h1 key={monster.id}>{monster.name}</h1>
+        }) }
+      </div>
+    )
+  }
 }
+
 
 export default App;
