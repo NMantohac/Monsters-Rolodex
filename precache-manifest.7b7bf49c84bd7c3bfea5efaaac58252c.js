913071c5b86cf666eaaa99f901b65dc8b6307ffc
Norberto Mantohac <nmantohac82098@gmail.com>
2020-07-05 14:57:13 -0700

    Updates

---
+++ precache-manifest.7b7bf49c84bd7c3bfea5efaaac58252c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4324e9371402e878903e29809c0341f8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "afde4d5fca0d1798e461",
+    "url": "/monsters-rolodex/static/css/main.1243183d.chunk.css"
+  },
+  {
+    "revision": "708d6393b709640cb167",
+    "url": "/monsters-rolodex/static/js/2.b3ff46a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b3ff46a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afde4d5fca0d1798e461",
+    "url": "/monsters-rolodex/static/js/main.b7dfd071.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
