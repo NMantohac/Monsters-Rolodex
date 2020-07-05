9dd21ad4dfad1a20eb63332f7c64d42793324348
Norberto Mantohac <nmantohac82098@gmail.com>
2020-07-05 14:12:44 -0700

    added search-box component, and used destructuring instead of props

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { CardList } from '../card-list/card-list.component';
+import { SearchBox } from '../search-box/search-box.component';
 import './App.styles.css';
 
 class App extends Component {
   state = {
-    monsters: []
+    monsters: [],
+    searchField: '',
   }
 
   async componentDidMount() {
@@ -22,14 +24,21 @@ class App extends Component {
   }
 
   render() {
-    // const { monsters } = this.state;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={(event) => this.setState({ searchField: event.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
 }
-
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
-export const CardList = (props) => {
+export const CardList = ({ monsters }) => {
   return (
     <div className='card-list'>
-      { props.monsters.map(monster => {
+      { monsters.map(monster => {
             return <Card key={monster.id} monster={monster} />
           }) }
     </div>
